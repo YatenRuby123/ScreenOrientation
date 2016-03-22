d06299500b52dd9951cbde858c15ca756c2bb905
Yatendra Pawar <rubyyatpaw2100@gmail.com>
2016-03-22 16:49:49 -0700

    Initial commit

---
+++ app/src/main/java/com/screenorientation/example/supershuttle/screenorientation/MainActivity.java
@@ -0,0 +1,57 @@
+package com.screenorientation.example.supershuttle.screenorientation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int score;
+    EditText et;
+    String etString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        score = 0;
+        et = (EditText)findViewById(R.id.editText1);
+}
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        etString = et.getText().toString();
+
+        outState.putString("edittext", etString);
+        outState.putInt("score", score);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        et.setText( savedInstanceState.getString("edittext"));
+
+        score = savedInstanceState.getInt("score");
+    }
+
+    public void performAction(View v) {
+
+        switch (v.getId()){
+            case R.id.bincrement:
+
+                score += 1;
+
+                break;
+            case R.id.bshowValue:
+
+                Toast.makeText(getApplicationContext(),"Your Score is " + score, Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
